f6ec4288ff865577e7a5edf19efc1c273206520d
Doanhaiduy <haiduytbt2k3@gmail.com>
2024-02-26 23:10:42 +0700

    finish TH_Bai1

---
+++ TH_Bai1_Login/src/duydh/thuchanh/ChuongTrinhChinh.java
@@ -0,0 +1,10 @@
+package duydh.thuchanh;
+
+public class ChuongTrinhChinh {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new LoginFrame().setVisible(true);
+	}
+
+}
